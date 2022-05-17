11991cb175b5f95b092f8db3ae6cd0086ae0bc02
Chris Torain <torain20@yahoo.com>
2022-05-17 00:09:32 -0500

    Update app.js

--- bananaberry/app.js
+++ bananaberry/app.js
@@ -1,8 +1,126 @@
 const wrapper = document.querySelector(".sliderWrapper")
 const menuItems = document.querySelectorAll(".menuItem")
 
+
+const products = [
+    {
+        id: 1,
+        title: "HANDMADE ORGANITE PIECES",
+        price: 119,
+        colors: [
+            {
+                code: "black",
+                img: "./img/air.png",
+            },
+            {
+                code: "darkblue",
+                img: "./img/air2.png",
+            },
+        ],
+    },
+    {
+        id: 2,
+        title: "CRYSTALS",
+        price: 149,
+        colors: [
+            {
+                code: "lightgray",
+                img: "./img/jordan.png",
+            },
+            {
+                code: "green",
+                img: "./img/jordan2.png",
+            },
+        ],
+    },
+    {
+        id: 3,
+        title: "SUNCATCHERS",
+        price: 109,
+        colors: [
+            {
+                code: "lightgray",
+                img: "./img/blazer.png",
+            },
+            {
+                code: "green",
+                img: "./img/blazer2.png",
+            },
+        ],
+    },
+    {
+        id: 4,
+        title: "ACCESSORIES",
+        price: 129,
+        colors: [
+            {
+                code: "black",
+                img: "./img/crater.png",
+            },
+            {
+                code: "lightgray",
+                img: "./img/crater2.png",
+            },
+        ],
+    },
+    {
+        id: 5,
+        title: "Hippie",
+        price: 99,
+        colors: [
+            {
+                code: "gray",
+                img: "./img/hippie.png",
+            },
+            {
+                code: "black",
+                img: "./img/hippie2.png",
+            },
+        ],
+    },
+];
+
+let chosenProduct = products[0];
+
+const currentProductImage = document.querySelector(".productImage");
+const currentProductTitle = document.querySelector(".productTitle");
+const currentProductPrice = document.querySelector(".productPrice");
+const currentProductColors = document.querySelectorAll(".color");
+const currentProductSizes = document.querySelectorAll(".size");
+
 menuItems.forEach((item, index) => {
     item.addEventListener("click", () => {
+        //change current slide
         wrapper.style.transform = `translateX(${-100 * index}vw)`;
+
+        //change the chosen product
+        chosenProduct = products[index]
+
+        //change text of current product
+        currentProductTitle.textContent = chosenProduct.title;
+        currentProductPrice.textContent = "$" + chosenProduct.price;
+        currentProductImage.src = chosenProduct.colors[0].img;
+
+        //assign new colors
+        currentProductColors.forEach((color, index) => {
+            color.style.backgroundColor = chosenProduct.colors[index].code;
+        })
     })
 })
+
+currentProductColors.forEach((color, index) => {
+    color.addEventListener("click", () => {
+        currentProductImage.src = chosenProduct.colors[index].img
+    });
+});
+
+currentProductSizes.forEach((size, index) => {
+    size.addEventListener("click", () => {
+        currentProductSizes.forEach((size) => {
+            size.style.backgroundColor = "white";
+            size.style.color = "black";
+        });
+        size.style.backgroundColor = "black";
+        size.style.color = "white";
+    });
+});
